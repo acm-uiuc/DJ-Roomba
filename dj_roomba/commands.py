d649e7ca483942c6240dddaf7aebb2ab412b0d5f
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-11-20 00:04:43 -0600

    added working joystick configuration

--- dj_roomba/commands.py
+++ dj_roomba/commands.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 
+START, SAFE, FULL = range(3)
 ROBOT = namedtuple('Robot', 'drive speed')
 
 def noop(state, _):
@@ -17,3 +18,7 @@ def control(state, val):
     if val == 0:
         return noop(state, val)
     return ('control',), ROBOT(0, 0)
+
+EVENT_TO_ACTION = {
+    'hat_0x': turn,
+}    
--- dj_roomba/joystick.py
+++ dj_roomba/joystick.py
@@ -2,20 +2,18 @@
 Handles joy stick communication to roomba
 """
 
-import imp
 import json
 from os.path import join
 
 import amqp
 import evdev
 
-from .commands import ROBOT
+from commands import ROBOT, EVENT_TO_ACTION, START
 
 HOST = 'localhost'
 DRIVE_QUEUE = 'drive'
 DEVICE = '/dev/input/event13'
-START, SAFE, FULL = range(3)
-CONFIG = 'controller.cfg'
+CONFIG = 'ps4.json'
 
 def main(host=HOST, drive_queue=DRIVE_QUEUE, device=DEVICE):
     """Creates connection to amqp on HOST to channel DRIVE, """
@@ -25,13 +23,14 @@ def main(host=HOST, drive_queue=DRIVE_QUEUE, device=DEVICE):
 
     # maybe add controller callibration??
     device = evdev.device.InputDevice(device) # get evdev device
-    config = imp.load_source(join('configs', CONFIG), CONFIG)
+    with open(join('configs', CONFIG), 'r') as fp:
+        config = json.load(fp)
 
-    state = ROBOT(0, 0, START)
+    state = ROBOT(0, START)
     for event in device.read_loop():
-        code, value = event.code, event.value
-        if code in config.EVENT_TO_ACTION.keys():
-            command, state = config.EVENT_TO_ACTION[event.code](state, value)
+        code, value = config[str(event.code)], event.value
+        if code in EVENT_TO_ACTION:
+            command, state = EVENT_TO_ACTION[code](state, value)
             msg = amqp.Message(json.dumps(command))
             channel.basic_publish(msg, routing_key=drive_queue)
 
