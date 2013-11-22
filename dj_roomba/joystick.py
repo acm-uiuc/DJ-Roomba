a3a18fd9a8f48436531bced1c7a3492de24de443
Marcell Vazquez-Chanlatte <mvc@linux.com>
2013-11-22 00:22:34 -0600

    added more command line arguments for joystick

--- dj_roomba/joystick.py
+++ dj_roomba/joystick.py
@@ -4,7 +4,6 @@ Handles joy stick communication to roomba
 
 import json
 from argparse import ArgumentParser, FileType
-from os.path import join, exists, expanduser
 
 import amqp
 import evdev
@@ -15,31 +14,35 @@ HOST = 'localhost'
 DRIVE_QUEUE = 'drive'
 DEVICE = '/dev/input/event13'
 
-def messages(events, config):
+def messages(events):
     state = ROBOT(0, START)
     for code, value in events:
         command, state = EVENT_TO_ACTION[code](state, value)
         yield amqp.Message(json.dumps(command))
-    
 
-def main(host=HOST, drive_queue=DRIVE_QUEUE, device=DEVICE):
+def main():
     """Creates connection to amqp on HOST to channel DRIVE, """
     parser = ArgumentParser()
     parser.add_argument('--config', '-c', required=True, type=FileType('r'))
+    parser.add_argument('--host', '-h', default=HOST)
+    parser.add_argument('--queue', '-q', default=DRIVE_QUEUE)
+    parser.add_argument('--device', '-d', default=None)
     args = parser.parse_args()
-    connection = amqp.Connection(host)
+
+    connection = amqp.Connection(args.host)
     channel = connection.channel()
-    channel.queue_declare(queue=drive_queue)
+    channel.queue_declare(queue=args.queue)
 
     # maybe add controller callibration??
-    device = evdev.device.InputDevice(device) # get evdev device
+    if args.device is None:
+        args.device = DEVICE
+    device = evdev.device.InputDevice(args.device) # get evdev device
     config = json.load(args.config)
     args.config.close()
 
     events = ((config[str(event.code)], event.value) for event
               in device.read_loop())
     events = (event for event in events if event[0] in EVENT_TO_ACTION)
-    for msg in messages(events, config):
+    for msg in messages(events):
         print(msg)
-        channel.basic_publish(msg, routing_key=drive_queue)
-
+        channel.basic_publish(msg, routing_key=args.queue)
