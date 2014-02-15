ab018996de088b5f42428419ac330d6f2f82a557
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-02-15 17:25:17 -0600

    diagram more or less generated

--- diagram.hs
+++ diagram.hs
@@ -2,6 +2,7 @@
 
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
+import Data.List
 
 driveR = "roomba drive"
 joystick = "joystick"
@@ -10,7 +11,7 @@ turret = "turret"
 sensorR = "roomba sensor"
 sensorT = "turret sensor"
 
-subsystems = [driveR, joystick, lcd, sensorR, sensorT, turret]
+subsystemNames = [driveR, joystick, lcd, sensorR, sensorT, turret]
 arrows = [(joystick, driveR)
          ,(joystick, lcd)
          ,(joystick, turret)
@@ -19,16 +20,26 @@ arrows = [(joystick, driveR)
 
 node :: String -> Diagram B R2
 node name = text name # scale 0.1 # fc white
-         <> circle 0.4 # fc green # named name
+         <> circle 0.2 # fc green # named name
 
-tournament :: [String] -> Diagram B R2
-tournament sysl = decorateTrail (regPoly n 1.2) (map node sysl)
-                  where n = length sysl
+queueH = hcat (replicate 5 (square 1 # fc blue)) 
+         # translate (r2 (-2, 0))
+         # scaleY (1/4) # scaleX (1/20) 
 
-queueH = scale (1/10) $ foldr1 (|||) (replicate 5 (square 1 # fc red))
 queueV = rotateBy (1/4) queueH
 
+directQ = vcat [node "x", strutY 0.2, queueV]
+
+subsystemGroup n = hcat $ intersperse (strutX 0.1) (replicate n directQ)
+
+joystickLCD = (node joystick === strutY 0.2 === node lcd) 
+              # translate (r2 (0, 0.3)) ||| queueV
+uiGroup =  (joystickLCD <> square 1 # dashing [0.2,0.05] 0)
+           # translate (r2 (1.5, 0))
+
+subsystems = subsystemGroup 3 ||| strutX 0.5 ||| subsystemGroup 3 
+
 example :: Diagram B R2
-example = queueH ||| queueV
+example = subsystems === strutY 0.3 === uiGroup
 
 main = mainWith $ example
