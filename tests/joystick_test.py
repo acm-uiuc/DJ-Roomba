5397c5e80c80a4205dd884395539aa9660d5fffe
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-03-07 22:33:26 -0600

    removed apply dict test since its not used anymore

--- tests/joystick_test.py
+++ tests/joystick_test.py
@@ -1,14 +1,6 @@
-from dj_roomba.joystick import Joystick, apply_dict
+from dj_roomba.joystick import Joystick
 from unittest import TestCase, main
 from unittest.mock import patch, MagicMock
-
-def test_apply_dict():
-    mapping = {'a': 1, 'c':2}
-    for case, result in [(['a'], [1]), (['a', 'b', 'c'], [1, 2]), (['b'], [])]:
-        yield check_dict, list(apply_dict(case, mapping)), result
-
-def check_dict(x, y):
-    assert x == y 
 
 class TestDriver(TestCase):
     def setUp(self):
