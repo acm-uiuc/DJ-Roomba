0952b12caba1dc705234cc8a328b9367b54872c2
Marcell Vazquez-Chanlatte <mvc@linux.com>
2015-03-09 15:01:32 -0500

    prototype code for controlling lights

--- audio_monitor/read_levels.py
+++ audio_monitor/read_levels.py
@@ -1,10 +1,37 @@
+from __future__ import print_function
+
 import amqp
 QUEUE = "audio_level.sensor"
 
+PINS = [60, 30, 31, 48, 3, 2, 15, 14, 66, 67]
+BINS = 127 // len(PINS)
+
 def drive_lights(cmd):
-    print(cmd.body)
+    val = cmd.body // len(PINS)
+    pin_vals = [(pin, 1) for i, pin in enumerate(PINS) if val > BINS*i]
+    
+    set_pins(pin_vals)
+
+
+def set_pins(pin_vals):
+    for pin, val in pin_vals:
+        with open("/sys/class/gpio/gpio{}/value".format(pin)) as value:
+            value.write(str(val))
+
+
+def setup_gpio():
+    for pin in PINS:
+        with open("/sys/class/gpio/export") as export:
+            export.write(str(pin))
+        with open("/sys/class/gpio/gpio{}/direction".format(pin)) as direction:
+            value.write("out")
+        with open("/sys/class/gpio/gpio{}/value".format(pin)) as value:
+            value.write("1")
+
 
 def main():
+    setup_gpio()
+
     connection = amqp.Connection("localhost")
     channel = connection.channel()
     channel.queue_declare(queue=QUEUE)
