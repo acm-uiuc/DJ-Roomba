355cf427ba72ac4003799cbe3afae5069deabba2
Marcell Vazquez-Chanlatte <mvc@linux.com>
2015-03-11 17:32:30 -0500

    change the way we talk to pins

--- dj_roomba/lights.py
+++ dj_roomba/lights.py
@@ -6,6 +6,8 @@ Listens on the audio_level.sensor queue
 
 from __future__ import print_function
 
+from subprocess import check_output
+
 import amqp
 QUEUE = "audio_level.sensor"
 
@@ -23,19 +25,19 @@ def drive_lights(cmd):
 def set_pins(pin_vals: "[(pin, val)]") -> "IO ()":
     """Set pins to corresponding value."""
     for pin, val in pin_vals:
-        with open("/sys/class/gpio/gpio{}/value".format(pin)) as value:
-            value.write(str(val))
+        echo("/sys/class/gpio/gpio{}/value".format(pin), val)
+
+def echo(val, path):
+    "echo val into file"
+    return check_output("echo {} > {}".format(val, path))
 
 
 def setup_gpio() -> "IO ()":
     """Setup pins to control lights."""
     for pin in PINS:
-        with open("/sys/class/gpio/export") as export:
-            export.write(str(pin))
-        with open("/sys/class/gpio/gpio{}/direction".format(pin)) as direction:
-            direction.write("out")
-        with open("/sys/class/gpio/gpio{}/value".format(pin)) as value:
-            value.write("1")
+        echo("/sys/class/gpio/export", str(pin))
+        echo("/sys/class/gpio/gpio{}/direction".format(pin), "out")
+        echo("/sys/class/gpio/gpio{}/value".format(pin), "1")
 
 
 def main():
